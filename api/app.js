b4674204f77161b2a48552dda707b3e67206384f
sumitkanojia22 <sumitkanojia2215@gmail.com>
2026-07-18 13:29:45 +0530

    Add all files

---
+++ api/app.js
@@ -0,0 +1,13 @@
+import express, { json } from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+const app = express();
+
+app.use(cors());
+
+app.use(helmet());
+
+app.use(express.json());
+
+export default app;
---
+++ api/server.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+import dotenv from "dotenv";
+
+import app from "./app.js";
+
+dotenv.config({ path: "./config.env" });
+
+const port = process.env.PORT || 8080;
+
+const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DB_PASSWORD);
+
+mongoose.connect(DB).then(() => {
+  console.log("DATABASE IS CONNECT SUCCESSFULLY");
+});
+
+app.listen(port, () => {
+  console.log(`LISTENING ON PORT:${port}`);
+});
